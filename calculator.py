841fde3f141db58cb29002fc7cb78e4560374110
nehaa <157296420+nehaadhawas@users.noreply.github.com>
2024-08-01 01:02:14 +0530

    🙌
    This is my first python program where I had build a basic calculator using operators.

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#calculator
+i=int(input("Enter the value"))
+j=int(input("Enter the value"))
+
+(i+j)
+print("Addition of i and j is:", i+j)
+(i-j)
+print("Subtraction of i and j is:", i-j)
+i/j
+print("Division of i and j is:", i/j)
+i*j
+print("Multiplication of i and j is:", i*j)
+i%j
+print("Modulus of i and j is:", i%j)
